301efb13b16aa83c009220f87c053c4aee76bd22
Mikaelia <mikaelagurney@gmail.com>
2018-04-26 04:20:45 +0000

    Add newline to pint

--- pint.c
+++ pint.c
@@ -9,7 +9,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		printf("L%d: can't pint, stack empty", line_number);
+		printf("L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
