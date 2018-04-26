2ef8d99f248e269267625cb29ce3625536549520
Mikaelia <mikaelagurney@gmail.com>
2018-04-26 06:56:58 +0000

    Add 2 element condition

--- swap.c
+++ swap.c
@@ -21,10 +21,11 @@ void swap(stack_t **stack, unsigned int line_number)
 	tmp1 = *stack;
 	tmp2 = (*stack)->next;
 
-	tmp1->next = tmp1->next->next;
+	tmp1->next = tmp2->next;
 	tmp1->prev = tmp2;
 	tmp2->prev = NULL;
 	tmp2->next = tmp1;
-	tmp1->next->prev = tmp1;
+	if (tmp1->next)
+		tmp1->next->prev = tmp1;
 	*stack = tmp2;
 }
